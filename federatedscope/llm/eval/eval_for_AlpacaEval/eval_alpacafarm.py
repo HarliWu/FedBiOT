e03fb28acd8ff92859b4a32667513d2806257965
Harli Wu <harliwu@outlook.com>
2024-06-15 02:11:27 +0800

    print for debug

--- federatedscope/llm/eval/eval_for_AlpacaEval/eval_alpacafarm.py
+++ federatedscope/llm/eval/eval_for_AlpacaEval/eval_alpacafarm.py
@@ -75,6 +75,7 @@ def evaluation(model, dataloader, choices):
         total = total + new_labels.size(0)
         correct = correct + batch_correct
 
+        print(correct, total, new_labels)
         test_batches.set_postfix({
             'correct': correct,
             'total': total,
